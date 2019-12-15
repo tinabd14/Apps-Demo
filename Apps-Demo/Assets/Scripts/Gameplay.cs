234d4e21684a2c79df519c591d989e8195c891b9
Tunar Mahmudov <tunardev14@gmail.com>
2019-12-15 22:43:42 +0300

    Scene Preparation
    Background images added
    JSON object accessed and parsed

---
+++ Apps-Demo/Assets/Scripts/Gameplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    JSONInventer myJSONinventer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myJSONinventer = new JSONInventer();
+        PrepareGrid();
+    }
+
+    private void PrepareGrid()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Apps-Demo/Assets/Scripts/JSONInventer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class JSONInventer
+{
+    private string jsonString;
+    private string topic;
+    private string[] words;
+    private double rewardCoin;
+    private Board board;
+
+    Puzzle puzzleJSON;
+    public JSONInventer()
+    {
+        InitializeVariablesFromJSON(puzzleJSON);
+    }
+
+    private void InitializeVariablesFromJSON(Puzzle puzzleJSON)
+    {
+        jsonString = File.ReadAllText(Application.dataPath + "/Resources/Puzzle.json");
+        puzzleJSON = JsonUtility.FromJson<Puzzle>(jsonString);
+
+        topic = puzzleJSON.topic;
+        words = puzzleJSON.words;
+        rewardCoin = puzzleJSON.rewardCoin;
+        Board board = puzzleJSON.board;
+    }
+
+    public string GetTopic()
+    {
+        return topic;
+    }
+
+    public string[] GetWords()
+    {
+        return words;
+    }
+
+    public double GetRewardCoin()
+    {
+        return rewardCoin;
+    }
+
+    public Board GetBoard()
+    {
+        return board;
+    }
+
+
+
+    [Serializable]
+    public class Puzzle
+    {
+        public string topic = null;
+        public string[] words = null;
+        public double rewardCoin = 0;
+        public Board board;
+    }
+
+    [Serializable]
+    public class Board
+    {
+        public int boardRow;
+        public int boardCol;
+        public Letter[] letters;
+    }
+
+    [Serializable]
+    public class Letter
+    {
+        public int rowIndex;
+        public int colIndex;
+        public string letter;
+    }
+
+}
